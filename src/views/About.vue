62dba941228e47e523c2df058db1cb26d69adf23
Deividas <deivis124@gmail.com>
2021-02-23 23:36:05 +0200

    Adds appear animation to About element using JS hooks and GSAP

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="about">
     <transition
-      name="fade"
       appear
       @before-enter="beforeEnter"
       @enter="enter"
       @after-enter="afterEnter"
-      @before-leave="beforeLeave"
-      @leave="leave"
-      @after-leave="afterLeave"
     >
-      <h1 v-if="showTitle">About</h1>
+      <h1>About</h1>
     </transition>
 
     <p>
@@ -36,44 +32,32 @@
 
 <script>
 import { ref } from "vue";
+import gsap from "gsap";
 
 export default {
   setup() {
-    const showTitle = ref(true);
-
     const beforeEnter = (el) => {
-      console.log("before enter", el);
+      console.log("before enter - set initial state");
+      el.style.transform = "translateY(-60px)";
+      el.style.opacity = 0;
     };
-    const enter = (el) => {
-      console.log("enter", el);
+    const enter = (el, done) => {
+      console.log("starting to enter - make transition");
+      gsap.to(el, {
+        y: 0,
+        opacity: 1,
+        duration: 3,
+        ease: "bounce.out",
+        onComplete: done,
+      });
     };
-    const afterEnter = (el) => {
-      el.style.color = "green";
-      console.log("after enter", el);
-      setTimeout(() => {
-        (showTitle.value = false), 2000;
-      }, 2000);
+    const afterEnter = () => {
+      console.log("after enter");
     };
-
-    const beforeLeave = (el) => {
-      el.style.color = "pink";
-      console.log("before leave", el);
-    };
-    const leave = (el) => {
-      console.log("leave", el);
-    };
-    const afterLeave = (el) => {
-      console.log("after leave", el);
-    };
-
     return {
-      showTitle,
       beforeEnter,
       enter,
       afterEnter,
-      beforeLeave,
-      leave,
-      afterLeave,
     };
   },
 };
@@ -83,18 +67,5 @@ export default {
 .about {
   max-width: 600px;
   margin: 20px auto;
-}
-
-.fade-enter-from {
-  opacity: 0;
-}
-.fade-enter-active {
-  transition: opacity 3s ease;
-}
-.fade-leave-to {
-  opacity: 0;
-}
-.fade-leave-active {
-  transition: opacity 3s ease;
 }
 </style>
